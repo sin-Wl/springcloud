4b0d25be5782001d9349dcd2d128477dbb1b9bbb
wenlei <18336274540@163.com>
2021-04-12 17:59:50 +0800

    服务熔断 --> Hystrix之circuit-break

--- cloud-provider-hystrix-payment8001/src/main/java/com/wenlei/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/wenlei/service/PaymentService.java
@@ -1,8 +1,10 @@
 package com.wenlei.service;
 
+import cn.hutool.core.util.IdUtil;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.concurrent.TimeUnit;
 
@@ -14,7 +16,7 @@ public class PaymentService {
         return "线程池:  "+Thread.currentThread().getName()+"  paymentInfo_OK,id:  "+id+"\t"+"O(∩_∩)O哈哈~";
     }
 
-    //服务降级注解
+    //====服务降级
     @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler"/*指定善后方法名*/,commandProperties = {
             @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds",value="3000") //定义等待时间，如果超出就降级-->执行paymentInfo_TimeOutHandler方法
     })
@@ -30,5 +32,26 @@ public class PaymentService {
     public String paymentInfo_TimeOutHandler(Integer id)
     {
         return "线程池:  "+Thread.currentThread().getName()+"  8001系统繁忙或者运行报错，请稍后再试,id:  "+id+"\t"+"o(╥﹏╥)o";
+    }
+
+
+    //=====服务熔断  -->  访问错误，进行服务降级，多次错误后（达到熔断条件-->错误率达到设定值），此时访问正确也会进行服务降级，在多次访问正确后，服务恢复（恢复调用链路）。
+    @HystrixCommand(fallbackMethod = "paymentCircuitBreaker_fallback",commandProperties = {
+            @HystrixProperty(name = "circuitBreaker.enabled",value = "true"),// 是否开启断路器
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold",value = "10"),// 请求次数
+            @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds",value = "10000"), // 时间窗口期
+            @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage",value = "60"),// 失败率 达到多少后跳闸
+            //  --------> 上列三行表示 ------> 在 10s 内 10次请求 有 6次请求 失败 就跳闸
+    })
+    public String paymentCircuitBreaker(@PathVariable("id") Integer id) {
+        if(id < 0) {
+            throw new RuntimeException("******id 不能负数");
+        }
+        String serialNumber = IdUtil.simpleUUID();
+
+        return Thread.currentThread().getName()+"\t"+"调用成功，流水号: " + serialNumber;
+    }
+    public String paymentCircuitBreaker_fallback(@PathVariable("id") Integer id) {
+        return "id 不能负数，请稍后再试，/(ㄒoㄒ)/~~   id: " +id;
     }
 }
