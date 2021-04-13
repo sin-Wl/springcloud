9d341f2675cd2c1151386880a361a8f0168d7386
wenlei <18336274540@163.com>
2021-04-13 14:23:23 +0800

    服务网关-->GateWay 之 路由（route）、断言（predicate）、过滤器（filter）

---
+++ cloud-gateway-gateway9527/src/test/java/T2.java
@@ -0,0 +1,15 @@
+import java.time.ZonedDateTime;
+
+
+public class T2
+{
+    public static void main(String[] args)
+    {
+        ZonedDateTime zbj = ZonedDateTime.now(); // 默认时区
+        System.out.println(zbj);
+
+        //2021-04-13T13:39:58.075+08:00[Asia/Shanghai]
+    }
+}
+
+
