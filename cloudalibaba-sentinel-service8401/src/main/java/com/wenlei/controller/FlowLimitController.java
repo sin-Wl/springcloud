ebccdb3129555d7eeb73778ef5ecd1c76085c11e
wenlei <18336274540@163.com>
2021-04-17 10:53:24 +0800

    cloudAlibaba-->Sentinel之热点Key，此部分查看参考笔记

--- cloudalibaba-sentinel-service8401/src/main/java/com/wenlei/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/wenlei/controller/FlowLimitController.java
@@ -1,7 +1,10 @@
 package com.wenlei.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.TimeUnit;
@@ -48,4 +51,18 @@ public class FlowLimitController {
         int age = 10/0;
         return "------testE 测试异常数";
     }
+
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey",blockHandler/*兜底方法*/ = "deal_testHotKey")  //可自定义降级处理方法
+    public String testHotKey(@RequestParam(value = "p1",required = false) String p1,
+                             @RequestParam(value = "p2",required = false) String p2) {
+        //int age = 10/0;
+        return "------testHotKey";
+    }
+
+    /*兜底方法*/
+    public String deal_testHotKey (String p1, String p2, BlockException exception) {
+        return "------deal_testHotKey,o(╥﹏╥)o";  //sentinel系统默认的提示：Blocked by Sentinel (flow limiting)
+    }
+
 }
